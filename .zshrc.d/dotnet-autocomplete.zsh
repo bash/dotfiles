a6480200c80e20395bfa1fabb46240c132c7f4ff
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-04-01 21:37:26 +0200

    Make dotnet autocomlete work on WSL too

--- .zshrc.d/dotnet-autocomplete.zsh
+++ .zshrc.d/dotnet-autocomplete.zsh
@@ -1,10 +1,20 @@
+# zsh parameter completion for the dotnet CLI
+
 # Copied from Oh My Zsh: https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/dotnet/dotnet.plugin.zsh
 # Licensed under the MIT license (c) 2009-2022 Robby Russell and contributors (https://github.com/ohmyzsh/ohmyzsh/contributors)
 # see https://github.com/ohmyzsh/ohmyzsh/blob/master/LICENSE.txt for the full license.
 
+if [[ -z "$DOTNET" ]]; then
+    if [[ -n "$WSLENV" ]]; then
+        DOTNET=dotnet.exe
+    else
+        DOTNET=dotnet
+    fi
+fi
+
 _dotnet_zsh_complete() 
 {
-  local completions=("$(dotnet complete "$words")")
+  local completions=("$($DOTNET complete "$words" | tr '\r\n' '\n')")
 
   # If the completion list is empty, just continue with filename selection
   if [ -z "$completions" ]
@@ -13,8 +23,8 @@ _dotnet_zsh_complete()
     return
   fi
 
-  # This is not a variable assignment, don't remove spaces!
+  # This is not a variable assigment, don't remove spaces!
   _values = "${(ps:\n:)completions}"
 }
 
-compdef _dotnet_zsh_complete dotnet
+compdef _dotnet_zsh_complete "$DOTNET"

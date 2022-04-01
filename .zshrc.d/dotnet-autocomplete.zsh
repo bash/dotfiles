8a6dca0e8957695500519b1756c9d68f89cab745
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-04-01 21:02:28 +0200

    Add dotnet autocomplete

---
+++ .zshrc.d/dotnet-autocomplete.zsh
@@ -0,0 +1,20 @@
+# Copied from Oh My Zsh: https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/dotnet/dotnet.plugin.zsh
+# Licensed under the MIT license (c) 2009-2022 Robby Russell and contributors (https://github.com/ohmyzsh/ohmyzsh/contributors)
+# see https://github.com/ohmyzsh/ohmyzsh/blob/master/LICENSE.txt for the full license.
+
+_dotnet_zsh_complete() 
+{
+  local completions=("$(dotnet complete "$words")")
+
+  # If the completion list is empty, just continue with filename selection
+  if [ -z "$completions" ]
+  then
+    _arguments '*::arguments: _normal'
+    return
+  fi
+
+  # This is not a variable assignment, don't remove spaces!
+  _values = "${(ps:\n:)completions}"
+}
+
+compdef _dotnet_zsh_complete dotnet

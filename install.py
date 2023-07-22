57164f55997f341414c0af7ed3ac8216d5564ff9
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-07-22 12:35:56 +0200

    Reformat install.py

--- install.py
+++ install.py
@@ -7,20 +7,22 @@ from pathlib import Path
 
 HOME = str(Path.home())
 
+
 def makedirs(path):
-    print(f'Creating directory \'{path}\'')
+    print(f"Creating directory '{path}'")
     os.makedirs(path, exist_ok=True)
 
+
 linked_files = [
-    '.editorconfig',
-    '.gitconfig',
-    '.gitignore',
-    '.sqliterc',
-    '.zshrc',
-    '.zshrc.d',
+    ".editorconfig",
+    ".gitconfig",
+    ".gitignore",
+    ".sqliterc",
+    ".zshrc",
+    ".zshrc.d",
 ]
 
-if platform.system() == 'Linux':
+if platform.system() == "Linux":
     linked_files += [
         ".config/autostart/1password.desktop",
         ".config/autostart/hidpi.desktop",
@@ -29,23 +31,23 @@ if platform.system() == 'Linux':
     ]
 
 touch_files = [
-    '.gitconfig.d/local.gitconfig',
-    '.gitconfig.d/github.gitconfig',
+    ".gitconfig.d/local.gitconfig",
+    ".gitconfig.d/github.gitconfig",
 ]
 
 for file in linked_files:
     src_path = path.join(path.dirname(path.realpath(__file__)), file)
     dest_path = path.join(HOME, file)
-    print(f'Creating symlink \'{src_path}\' -> \'{dest_path}\'')
+    print(f"Creating symlink '{src_path}' -> '{dest_path}'")
     try:
         symlink(src_path, dest_path)
     except FileExistsError:
-        print(f'WARN: Symlink destination \'{dest_path}\' already exists, skipping...')
+        print(f"WARN: Symlink destination '{dest_path}' already exists, skipping...")
 
-makedirs(path.join(HOME, '.gitconfig.d'))
+makedirs(path.join(HOME, ".gitconfig.d"))
 
 for file in touch_files:
     dest_path = path.join(HOME, file)
-    print(f'Touching \'{dest_path}\'...')
-    with open(dest_path, 'a'):
+    print(f"Touching '{dest_path}'...")
+    with open(dest_path, "a"):
         pass

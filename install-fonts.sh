f99838e5a295358769daa74b47b4fbdaf870a69f
Tau <4602612+bash@users.noreply.github.com>
2023-12-09 23:50:17 +0100

    Add font installer

---
+++ install-fonts.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.local/share/fonts
+wget2 https://github.com/bash/sieben-7/releases/download/1.0.0/Sieben-7-Regular.ttf -O ~/.local/share/fonts/
+
+# Clears the font cache
+fc-cache -f

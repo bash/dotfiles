833b59d99729a97a44c80e994e608e5f4610e597
Tau <4602612+bash@users.noreply.github.com>
2023-12-11 21:00:07 +0100

    Simplify sqlite3 alias

--- .config/fish/functions/sqlite3.fish
+++ .config/fish/functions/sqlite3.fish
@@ -1,8 +1,6 @@
-set original_command (which sqlite3)
-if not set -q XDG_CONFIG_HOME
-    set XDG_CONFIG_HOME $HOME/.config
-end
-
 function sqlite3
-    $original_command --init "$XDG_CONFIG_HOME/sqlite3/sqliterc" $argv
+    if not set -q XDG_CONFIG_HOME
+        set XDG_CONFIG_HOME $HOME/.config
+    end
+    command sqlite3 --init "$XDG_CONFIG_HOME/sqlite3/sqliterc" $argv
 end

722a0c3fe5f50974ff607fc6b6e36ca428a10882
Tau <git@tau.garden>
2024-01-07 21:10:24 +0100

    Fix secret reference

--- .config/fish/conf.d/rust.fish
+++ .config/fish/conf.d/rust.fish
@@ -1,2 +1,2 @@
 fish_add_path --prepend --path "$HOME/.cargo/bin"
-set --export CARGO_REGISTRY_TOKEN "op://Software Development/crates.io API Token/credential"
+set --export CARGO_REGISTRY_TOKEN "op://Development/crates.io-token/credential"

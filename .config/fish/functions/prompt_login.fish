0848b8e0105f67bfd7636e009ed63492a2ebb53a
Tau <git@tau.garden>
2024-03-25 12:11:10 +0100

    Show toolbox in prompt

---
+++ .config/fish/functions/prompt_login.fish
@@ -0,0 +1,14 @@
+function prompt_login --description "display user name for the prompt"
+    # If we're running inside toolbox, show icon.
+    if set -q TOOLBOX_PATH
+        echo -n -s (set_color 613583) "⬢ " (set_color normal)
+    end
+
+    # If we're running via SSH, change the host color.
+    set -l color_host $fish_color_host
+    if set -q SSH_TTY; and set -q fish_color_host_remote
+        set color_host $fish_color_host_remote
+    end
+
+    echo -n -s (set_color $fish_color_user) "$USER" (set_color normal) @ (set_color $color_host) (prompt_hostname) (set_color normal)
+end

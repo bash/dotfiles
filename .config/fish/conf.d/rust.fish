571cc06a66905296a223f820ea6f1e8469f4d2da
Tau <git@tau.garden>
2024-03-25 18:25:19 +0100

    Make sure cargo bin is at start of PATH

--- .config/fish/conf.d/rust.fish
+++ .config/fish/conf.d/rust.fish
@@ -1,2 +1,4 @@
-fish_add_path --prepend --path "$HOME/.cargo/bin"
+# The --move is really important in cases where the .cargo/bin is
+# already in the path but in the wrong place (this happens to me in VSCode on my Mac)
+fish_add_path --prepend --move --path "$HOME/.cargo/bin"
 set --export CARGO_REGISTRY_TOKEN "op://Development/crates.io-token/credential"

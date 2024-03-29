e56d2b868a9ab633a9d4bd561381a1b8d6f5ba27
Tau <git@tau.garden>
2024-03-29 12:13:21 +0100

    Migrate wireplumber config

--- .config/wireplumber/bluetooth.lua.d/51-disable-handsfree.lua
+++
@@ -1,6 +0,0 @@
--- The file starts with 51- because we need to be loaded after 50-bluez-config.lua
--- which configures `bluez_monitor.properties`.
-
--- See: https://pipewire.pages.freedesktop.org/wireplumber/configuration/bluetooth.html
--- This property by default also includes the hfp_ag (Handsfree) role, which we don't want.
-bluez_monitor.properties["bluez5.headset-roles"] = "[ hsp_hs ]"

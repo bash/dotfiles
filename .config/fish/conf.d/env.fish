66d3f25b9399bbf0082cb70bb45c6bbea8dc478f
Tau Gärtli <git@tau.garden>
2024-04-15 14:35:38 +0200

    Configure paging to use bat

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -1,3 +1,2 @@
 fish_add_path --prepend --path "$HOME/.local/bin"
 set --export EDITOR vim
-set --export LESS -r
---
+++ .config/fish/conf.d/pager.fish
@@ -0,0 +1,6 @@
+set --export LESS -R
+set --export LESSOPEN '|lesspipe.sh %s'
+set --export LESSCOLORIZER bat
+
+set --export MANROFFOPT '-c'
+set --export MANPAGER 'sh -c \'col -bx | bat -l man -p\''

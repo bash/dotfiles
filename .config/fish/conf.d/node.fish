2aee12c4b58bc3d2e95ea353c2a8b41abfccfa2f
Tau Gärtli <git@tau.garden>
2024-05-14 22:32:06 +0200

    Make npm less chatty

---
+++ .config/fish/conf.d/node.fish
@@ -0,0 +1,7 @@
+# Disable update notifications for NPM
+# https://docs.npmjs.com/cli/v6/using-npm/config#update-notifier
+set --export NPM_CONFIG_UPDATE_NOTIFIER false
+
+# Disable funding notifications
+# https://docs.npmjs.com/cli/v6/using-npm/config#fund
+set --export NPM_CONFIG_FUND false

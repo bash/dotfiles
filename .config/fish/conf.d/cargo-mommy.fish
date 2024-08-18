260e486c3f1e1924b4ff90a8302f61886c2833b2
Tau Gärtli <git@tau.garden>
2024-08-18 18:04:08 +0200

    Extract env vars back out

---
+++ .config/fish/conf.d/cargo-mommy.fish
@@ -0,0 +1,7 @@
+if status is-interactive && type -q cargo-mommy
+    set -x CARGO_MOMMYS_LITTLE 'pet/baby/toy'
+    set -x CARGO_MOMMYS_PRONOUNS 'their'
+    set -x CARGO_MOMMYS_ROLES 'daddy'
+    set -x CARGO_MOMMYS_MOODS 'chill/ominous/thirsty/yikes'
+    set -x CARGO_MOMMYS_PARTS 'sugar/milk'
+end
--- .config/fish/functions/cargo.fish
+++ .config/fish/functions/cargo.fish
@@ -1,11 +1,5 @@
 if type -q cargo && type -q cargo-mommy
     function cargo
-        set -x CARGO_MOMMYS_LITTLE 'pet/baby/toy'
-        set -x CARGO_MOMMYS_PRONOUNS 'their'
-        set -x CARGO_MOMMYS_ROLES 'daddy'
-        set -x CARGO_MOMMYS_MOODS 'chill/ominous/thirsty/yikes'
-        set -x CARGO_MOMMYS_PARTS 'sugar/milk'
-
         switch $argv[1]
             case '+*'
                 command cargo "$argv[1]" mommy $argv[2..(count $argv)]

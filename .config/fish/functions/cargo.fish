29cb179919c82d40d9353189f89ae0b89896bf9d
Tau Gärtli <git@tau.garden>
2024-09-26 18:11:31 +0200

    Add env var to disable cargo-mommy

--- .config/fish/functions/cargo.fish
+++ .config/fish/functions/cargo.fish
@@ -1,10 +1,14 @@
 if type -q cargo && type -q cargo-mommy
     function cargo
-        switch $argv[1]
-            case '+*'
-                command cargo "$argv[1]" mommy $argv[2..(count $argv)]
-            case '*'
-                command cargo mommy $argv
+        if not set -q NO_CARGO_MOMMY
+            switch $argv[1]
+                case '+*'
+                    command cargo "$argv[1]" mommy $argv[2..(count $argv)]
+                case '*'
+                    command cargo mommy $argv
+            end
+        else
+            command cargo $argv
         end
     end
 end

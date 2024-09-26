24e70623a8634ab064f3c5f82bf5c1191afb5b2e
Tau Gärtli <git@tau.garden>
2024-09-26 19:29:05 +0200

    Simplify color scheme config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,6 +9,15 @@ local function scheme_for_appearance(appearance)
   end
 end
 
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
 local font_family = 'Departure Mono'
 
 config.window_background_opacity = 0.9
@@ -17,21 +26,13 @@ config.macos_window_background_blur = 30
 config.font = wezterm.font(font_family)
 config.font_size = 15
 
-config.window_decorations = 'RESIZE'
+config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
 
 config.window_frame = {
   font = wezterm.font({ family = font_family, weight = 'Bold' }),
   font_size = 14,
 }
 
-wezterm.on('window-config-reloaded', function(window, pane)
-  local overrides = window:get_config_overrides() or {}
-  local appearance = window:get_appearance()
-  local scheme = scheme_for_appearance(appearance)
-  if overrides.color_scheme ~= scheme then
-    overrides.color_scheme = scheme
-    window:set_config_overrides(overrides)
-  end
-end)
+config.color_scheme = scheme_for_appearance(get_appearance())
 
 return config

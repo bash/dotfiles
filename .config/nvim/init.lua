2fa2cdb8ca81b6bfa18b0faa5709171f72ed5231
Tau Gärtli <git@tau.garden>
2024-09-28 17:20:47 +0200

    Use treesitter for lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -40,7 +40,7 @@ require("lazy").setup({
   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate", config = function ()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
-            ensure_installed = { "just" },
+            ensure_installed = { "just", "lua" },
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },

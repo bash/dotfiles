35b9b34f6fe11531917f113a897d5848df1ebfff
Tau Gärtli <git@tau.garden>
2024-11-03 14:01:41 +0100

    Remove unused config file

--- .config/fish/conf.d/eza.fish
+++
@@ -1,6 +0,0 @@
-# Use 'dim' instead of dark gray for punctuation
-# making it work for dark backgrounds
-# .. so apparently this is an issue with the 'Horizon'
-# theme that I'm using in Ptyxis (other themes correctly adapt
-# dark gray to something legible *le sigh*).
-set -x EZA_COLORS 'xx=2'

d6e26879a4bd78c34b8abe2078af101f92fc03b3
Tau Gärtli <git@tau.garden>
2024-11-20 17:28:16 +0100

    Remove man pager

--- .config/fish/conf.d/pager.fish
+++ .config/fish/conf.d/pager.fish
@@ -1,12 +1,7 @@
-set -x LESS '-F'
+set -x LESS '-FR'
 set -x LESSOPEN '|lesspipe.sh %s'
 
 if type -q bat
     set -x LESSCOLORIZER bat
 end
 
-set -x MANROFFOPT '-c'
-
-if type -q bat
-    set -x MANPAGER 'sh -c \'col -bx | bat -l man -p\''
-end

24f74af1841d219d7f2237d625a5956f42b8d513
Tau Gärtli <git@tau.garden>
2025-01-13 18:12:45 +0100

    Setup windows dotfiles

---
+++ windows/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,90 @@
+﻿Set-Alias -Name subl -Value 'C:\Program Files\Sublime Text\subl.exe'
+
+# From <https://superuser.com/a/1748972>:
+Set-PSReadLineOption -Colors @{
+  Command            = 'Black'
+  Number             = 'DarkGray'
+  Member             = 'DarkGray'
+  Operator           = 'DarkGray'
+  Type               = 'DarkGray'
+  Variable           = 'DarkGreen'
+  Parameter          = 'DarkGreen'
+  ContinuationPrompt = 'DarkGray'
+  Default            = 'DarkGray'
+}
+
+
+# History search is bound to F8/Shift-F8 by default
+Set-PSReadLineKeyHandler -Chord UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Chord DownArrow -Function HistorySearchForward
+
+# CTRL-a and CTRL-e for start and end of line
+Set-PSReadLineKeyHandler -Chord Ctrl+a -Function BeginningOfLine
+Set-PSReadLineKeyHandler -Chord Ctrl+e -Function EndOfLine
+
+function prompt {
+    Write-Host
+
+    $folderName = if ((Get-Location).Path -eq $HOME) { '~' } else { Split-Path -Path (Get-Location) -Leaf }
+    Write-Host -NoNewline $folderName
+    Write-Git-Branch-Indicator
+
+    Write-Host
+    return "▷ "
+}
+
+function env {
+    gci env:* | Sort-Object name
+}
+
+function Write-Git-Branch-Indicator {
+    $branch = git branch --show-current
+    if ($LastExitCode -eq 0)
+    {
+        Write-Host -NoNewline " ("
+
+        if ($branch -eq $null) {
+            Write-Host -NoNewline -ForegroundColor Red "$(git rev-parse --short HEAD)..."
+        }
+        else {
+            Write-Host -NoNewline -ForegroundColor Green $branch
+            Write-Git-Dirty-State
+            Write-Host -NoNewline (Get-Git-Upstream-Indicator)
+        }
+
+        Write-Host -NoNewline ")"
+    }
+}
+
+function Write-Git-Dirty-State {
+    git diff --no-ext-diff --quiet
+    $dirty = $LastExitCode -eq 1
+
+    git diff --no-ext-diff --cached --quiet
+    $hasStagedFiles = $LastExitCode -eq 1
+
+    if ($dirty -or $hasStagedFiles) {
+        Write-Host -NoNewLine ' '
+    }
+
+    if ($dirty) {
+        Write-Host -NoNewLine -ForegroundColor Red '*'
+    }
+
+    if ($hasStagedFiles) {
+        Write-Host -NoNewLine -ForegroundColor Green '+'
+    }
+}
+
+function Get-Git-Upstream-Indicator {
+    $upstream = git rev-parse --abbrev-ref --symbolic-full-name '@{u}'
+    $count = git rev-list --count --left-right "$upstream...HEAD"
+
+    switch -Wildcard ($count) {
+        "" { return "" }
+        "0`t0" { return "=" }
+        "0`t*" { return ">" }
+        "*`t0" { return "<" }
+        default { return "<>" }
+    }
+}
---
+++ windows/install.py
@@ -0,0 +1,76 @@
+# /// script
+# dependencies = [
+#     "termcolor~=2.4.0",
+# ]
+# ///
+
+import os
+from os import symlink, path, unlink
+from pathlib import Path
+from subprocess import check_call
+from termcolor import colored
+
+
+def install():
+    update_submodules()
+
+    with open(path.join(path.dirname(__file__), "symlinks.txt")) as f:
+        symlinks = [parse_symlink(line) for line in f.readlines()]
+    symlink_files(symlinks)
+
+
+def update_submodules():
+    check_call(["git", "submodule", "update", "--init", "--recursive"])
+
+def parse_symlink(input: str) -> tuple[str, str]:
+    input = input.strip()
+    if "->" in input:
+        link, dest = (path.strip() for path in input.split("->", maxsplit=2))
+        return (link, dest)
+    else:
+        return (input, input)
+
+def symlink_files(files: list[(str, str)]) -> None:
+    for link_name, dest_name in files:
+        src_path = path.realpath(path.join(path.dirname(path.realpath(__file__)), dest_name))
+        link_path = path.join(HOME, link_name)
+        if not path.exists(src_path):
+            print(f"{colored('error', color='red')}: source file '{pretty_path(src_path)}' does not exist")
+            exit(1)
+        makedirs(path.dirname(link_path))
+        try:
+            print(
+                f"Symlink '{pretty_path(link_path)}' -> '{pretty_path(src_path)}'",
+                end="",
+            )
+            update = path.islink(link_path)
+            if update:
+                unlink(link_path)
+            symlink(path.relpath(src_path, start=path.dirname(link_path)), link_path)
+            if update:
+                print(f" [{colored('ok, updated', color='green')}]")
+            else:
+                print(f" [{colored('ok, new', color='green')}]")
+        except FileExistsError:
+            print(f" [{colored('skipped', color='yellow')}]")
+
+
+HOME = str(Path.home())
+DOTFILES = path.realpath(path.join(path.dirname(path.realpath(__file__)), ".."))
+
+
+def makedirs(path):
+    os.makedirs(path, exist_ok=True)
+
+
+def pretty_path(path: str) -> str:
+    if path.startswith(DOTFILES):
+        return path.replace(DOTFILES, "{dotfiles}", 1)
+    elif path.startswith(HOME):
+        return path.replace(HOME, "~", 1)
+    else:
+        return path
+
+
+if __name__ == "__main__":
+    install()

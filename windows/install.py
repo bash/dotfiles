cdb4ca734a1cf84218a0c50616aafea6ef1ca7e3
Tau Gärtli <git@tau.garden>
2025-01-23 13:12:41 +0100

    Set environment variables on windows

--- windows/install.py
+++ windows/install.py
@@ -8,6 +8,7 @@ import os
 from os import symlink, path, unlink
 from pathlib import Path
 from subprocess import check_call
+from typing import Optional
 from termcolor import colored
 
 
@@ -18,9 +19,14 @@ def install():
         symlinks = [parse_symlink(line) for line in f.readlines()]
     symlink_files(symlinks)
 
+    with open(path.join(path.dirname(__file__), "symlinks.txt")) as f:
+        env_vars = [parse_env_var(line) for line in f.readlines() if line is not None]
+    set_env_vars(env_vars)
+
 
 def update_submodules():
     check_call(["git", "submodule", "update", "--init", "--recursive"])
+
 
 def parse_symlink(input: str) -> tuple[str, str]:
     input = input.strip()
@@ -29,6 +35,17 @@ def parse_symlink(input: str) -> tuple[str, str]:
         return (link, dest)
     else:
         return (input, input)
+
+
+def parse_env_var(input: str) -> Optional[tuple[str, str]]:
+    input = input.strip()
+    if len(input) == 0 or input.startswith("#"):
+        return None
+    parts = input.split("=", maxsplit=1)
+    if len(parts) != 2:
+        raise Exception(f"Invalid environment variable '{input}'")
+    return (parts[0], parts[1])
+
 
 def symlink_files(files: list[tuple[str, str]]) -> None:
     for link_name, dest_name in files:
@@ -53,6 +70,11 @@ def symlink_files(files: list[tuple[str, str]]) -> None:
                 print(f" [{colored('ok, new', color='green')}]")
         except FileExistsError:
             print(f" [{colored('skipped', color='yellow')}]")
+
+
+def set_env_vars(env_vars: list[tuple[str, str]]) -> None:
+    for (name, value) in env_vars:
+        check_call(["setx", name, value])
 
 
 HOME = str(Path.home())

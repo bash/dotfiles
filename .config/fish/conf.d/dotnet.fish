aca6ee0d180e400d4cc270f7e52c603af6143ddf
Tau Gärtli <git@tau.garden>
2025-01-23 12:28:03 +0100

    Update dotnet config

--- .config/fish/conf.d/dotnet.fish
+++ .config/fish/conf.d/dotnet.fish
@@ -9,4 +9,11 @@ if type -q dotnet
     set -s DOTNET_ADD_GLOBAL_TOOLS_TO_PATH 0
     # Environment for host
     set -x DOTNET_ENVIRONMENT Development
+
+    # Stop `Verify` from automatically opening a diff tool
+    # I want to use `dotnet verify review` instead.
+    # See: https://github.com/VerifyTests/DiffEngine?tab=readme-ov-file#disable-for-a-machineprocess
+    set -x DiffEngine_Disabled true
+
+    fish_add_path --path "$HOME/.dotnet/tools"
 end

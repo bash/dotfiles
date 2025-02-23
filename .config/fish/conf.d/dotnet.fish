4d8b1b4afd911bea22d51a2a67261a6f01ea0250
Tau Gärtli <git@tau.garden>
2025-02-23 20:16:36 +0100

    Disable emojis for dotnet watch

--- .config/fish/conf.d/dotnet.fish
+++ .config/fish/conf.d/dotnet.fish
@@ -9,6 +9,8 @@ if type -q dotnet
     set -s DOTNET_ADD_GLOBAL_TOOLS_TO_PATH 0
     # Environment for host
     set -x DOTNET_ENVIRONMENT Development
+    # Disable emojis for `dotnet watch`
+    set -x DOTNET_WATCH_SUPPRESS_EMOJIS 1
 
     # Stop `Verify` from automatically opening a diff tool
     # I want to use `dotnet verify review` instead.

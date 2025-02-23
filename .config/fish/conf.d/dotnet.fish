30aa18d7822f8a2ee9da28db8fa57b7d768f0a7c
Tau Gärtli <git@tau.garden>
2025-02-23 20:16:53 +0100

    Give up trying to make NuGet follow XDG

--- .config/fish/conf.d/dotnet.fish
+++ .config/fish/conf.d/dotnet.fish
@@ -18,15 +18,4 @@ if type -q dotnet
     set -x DiffEngine_Disabled true
 
     fish_add_path --path "$HOME/.dotnet/tools"
-
-    if not set -q XDG_CACHE_HOME
-        set XDG_CACHE_HOME "$HOME/.cache"
-    end
-
-    # Try to make NuGet follow XDG conventions.
-    # Unfortunately, it still creates ~/.nuget/NuGet/NuGet.Config.
-    # See: <https://github.com/NuGet/Home/issues/13027>
-    set -x NUGET_PACKAGES "$XDG_CACHE_HOME/NuGet/packages"
-    set -x NUGET_HTTP_CACHE_PATH "$XDG_CACHE_HOME/NuGet/http-cache"
-    set -x NUGET_PLUGINS_CACHE_PATH "$XDG_CACHE_HOME/NuGet/plugin-cache"
 end

af6f15e4eb463cd133a10789e1d4abcf0c9a05c7
Tau Gärtli <git@tau.garden>
2025-03-27 19:58:35 +0100

    Add kdl

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -40,7 +40,7 @@ require("lazy").setup({
   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate", config = function ()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
-            ensure_installed = { "just", "lua" },
+            ensure_installed = { "just", "lua", "kdl" },
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },

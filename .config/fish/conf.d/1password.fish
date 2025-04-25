4bd6b00d8c12c24eb9838cfac1587891a437b157
Tau Gärtli <git@tau.garden>
2025-04-25 18:09:32 +0200

    Configure 1Password SSH Agent

--- .config/fish/conf.d/1password.fish
+++ .config/fish/conf.d/1password.fish
@@ -1,3 +1,7 @@
 if test (uname) = "Darwin"
     set -x SSH_AUTH_SOCK "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
 end
+
+if test -z "$SSH_TTY" && test -S "$HOME/.1password/agent.sock"
+    set -x SSH_AUTH_SOCK "$HOME/.1password/agent.sock"
+end

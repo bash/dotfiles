05635c1a7f23041cb1560db46909393152daf8c7
Tau Gärtli <git@tau.garden>
2025-05-02 17:56:51 +0200

    Use official manifest as template

--- firefox/setup-1password-firefox-native-messaging.sh
+++ firefox/setup-1password-firefox-native-messaging.sh
@@ -26,19 +26,6 @@ chmod +x "$HOME/.var/app/$flatpak_name/data/bin/1Password-BrowserSupport"
 
 # Configure native messaging for our shim
 mkdir -p "$HOME/.var/app/$flatpak_name/.mozilla/native-messaging-hosts"
-# shellcheck disable=SC2016
-echo '{
-    "name": "com.1password.1password",
-    "description": "1Password BrowserSupport",
-    "path": "$HOME/.var/app/org.mozilla.firefox/data/bin/1password-wrapper.sh",
-    "type": "stdio",
-    "allowed_extensions": [
-        "{0a75d802-9aed-41e7-8daa-24c067386e82}",
-        "{25fc87fa-4d31-4fee-b5c1-c32a7844c063}",
-        "{d634138d-c276-4fc8-924b-40a0ea21d284}"
-    ]
-}' \
-    | jq --arg path "$HOME/.var/app/$flatpak_name/data/bin/1Password-BrowserSupport" '.path = $path' \
+jq --arg path "$HOME/.var/app/$flatpak_name/data/bin/1Password-BrowserSupport" '.path = $path' \
+    < "$HOME/.mozilla/native-messaging-hosts/com.1password.1password.json" \
     > "$HOME/.var/app/$flatpak_name/.mozilla/native-messaging-hosts/com.1password.1password.json"
-
-
